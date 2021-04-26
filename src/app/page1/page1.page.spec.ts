a12bc431170f92bbbd0289a9528030f90e6a1647
sivakumar966 <sivakumar966@gmail.com>
2021-04-26 11:56:30 +0530

    todolist

--- src/app/page1/page1.page.spec.ts
+++ src/app/page1/page1.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { Page1Page } from './page1.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('Page1Page', () => {
+  let component: Page1Page;
+  let fixture: ComponentFixture<Page1Page>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ Page1Page ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(Page1Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
---
+++ src/app/page1/page1.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.page.html',
+  styleUrls: ['./page1.page.scss'],
+})
+export class Page1Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+   mytext  = '';
+   
+   myList = [];
+
+   AddList() {
+    this.myList.push(this.mytext);
+    this.mytext = '';
+  }
+
+}
